469d52a9feb75d9f0fe5e83d3e021eae1301fd9f
LeonidMakhov <makhov-leonid999@yandex.ru>
2021-11-19 17:29:38 +0300

    add initialCounters

--- src/App.js
+++ src/App.js
@@ -7,46 +7,47 @@ const initialCounters = [
     {id: 3, value: 300},
     {id: 4, value: 400},
     {id: 5, value: 500},
-
+    {id: 6, value: 800},
+    {id: 7, value: 900},
+    {id: 8, value: 150},
+    {id: 9, value: 250}
 ]
 
 function App() {
     const [counters, setCounters] = useState(initialCounters);
-const clickPlus = (id) => {
-    const update = counters.map(el => el.id === id ? {...el, value: el.value + 1} : el);
-    setCounters(update);
-}
-const clickMinus = (id) => {
-    const update = counters.map(el => el.id === id ? {...el, value: el.value - 1} : el);
-    setCounters(update);
-}
-const clickReset = (id) => {
-    const update = counters.map(el => el.id === id ? {...el, value: el.value = 0} : el);
-    setCounters(update);
-}
-const clickDelete = (id) => {
-    const deleteCount = counters.filter(el => el.id !== id);
-    setCounters(deleteCount);
-}
-const addCounter = () => {
-    const update = [...counters, {id: Math.random(), value: Math.ceil(Math.random()*1000)}];
-    setCounters(update);
-}
+    const clickPlus = (id) => {
+        const update = counters.map(el => el.id === id ? {...el, value: el.value + 1} : el);
+        setCounters(update);
+    }
+    const clickMinus = (id) => {
+        const update = counters.map(el => el.id === id ? {...el, value: el.value - 1} : el);
+        setCounters(update);
+    }
+    const clickReset = (id) => {
+        const update = counters.map(el => el.id === id ? {...el, value: el.value = 0} : el);
+        setCounters(update);
+    }
+    const clickDelete = (id) => {
+        const deleteCount = counters.filter(el => el.id !== id);
+        setCounters(deleteCount);
+    }
+    const addCounter = () => {
+        const update = [...counters, {id: Math.random(), value: Math.ceil(Math.random() * 1000)}];
+        setCounters(update);
+    }
 
     return (
         <div>
             <el>COUNTERS</el>
-            {counters.map(el => (<div key = {el.id}>
+            {counters.map(el => (<div key={el.id}>
                     <button onClick={() => clickPlus(el.id)}>Plus</button>
                     {el.value}
                     <button onClick={() => clickMinus(el.id)}>Minus</button>
                     <button onClick={() => clickReset(el.id)}>Reset</button>
                     <button onClick={() => clickDelete(el.id)}>Delete</button>
                 </div>
-             ))}
+            ))}
             <button onClick={addCounter}>Add</button>
-
-
         </div>
 
     );
